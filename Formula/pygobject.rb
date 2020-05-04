24c1c2a9585ff12d42cbd2e2c9077ef3d101de87
Ryan Robeson <ryan.robeson@gmail.com>
2020-05-04 12:47:05 -0400

    python@2: Add python@2 since it was removed from homebrew/core

--- Formula/pygobject.rb
+++ Formula/pygobject.rb
@@ -15,7 +15,7 @@ class Pygobject < Formula
 
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "python@2"
+  depends_on "ryan-robeson/gimp/python@2"
 
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/pygobject/2.28.7.diff"
